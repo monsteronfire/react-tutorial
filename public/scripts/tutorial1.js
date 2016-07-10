02b13f513c1cd04c6187dd7fb5e77e7d09670628
monsteronfire <monsteronfire@gmail.com>
2016-07-10 16:27:58 +0800

    Add data object * Also add temp files to gitignore

--- public/scripts/tutorial1.js
+++ public/scripts/tutorial1.js
@@ -1,3 +1,8 @@
+var data = [
+  { id: 1, author: "Pete Hunt", text: "This is one comment" },
+  { id: 2, author: "Jordan Walke", text: "This is *another* comment" }
+];
+
 var Comment = React.createClass({
   rawMarkup: function() {
     var md = new Remarkable();
@@ -21,7 +26,7 @@ var CommentBox = React.createClass({
     return (
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentList />
+        <CommentList data={this.props.data} />
         <CommentForm />
       </div>
     );
@@ -30,10 +35,16 @@ var CommentBox = React.createClass({
 
 var CommentList = React.createClass({
   render: function() {
+    var commentNodes = this.props.data.map(function(comment) {
+      return (
+        <Comment author={comment.author} key={comment.id}>
+          {comment.text}
+        </Comment>
+      );
+    });
     return (
       <div className="commentList">
-        <Comment author="Pete Hunt">This is a comment.</Comment>
-        <Comment author="Jordan Walke">This is *another* comment</Comment>
+        {commentNodes}
       </div>
     );
   }
@@ -50,6 +61,6 @@ var CommentForm = React.createClass({
 });
 
 ReactDOM.render(
-  <CommentBox />,
+  <CommentBox data={data} />,
   document.getElementById('content')
 );
